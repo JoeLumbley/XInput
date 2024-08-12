495a71ff2e7e6ab178f244d2126fb60c187396ca
Joseph Lumbley <joelumbley16@gmail.com>
2024-08-12 14:39:38 -0500

    Clean up

--- XInput/Form1.vb
+++ XInput/Form1.vb
@@ -79,7 +79,6 @@ Public Class Form1
     'Set the end of the thumbstick neutral zone to 1/2 over.
     Private Const NeutralEnd As Short = 16384 '16,383.5 = 32,767 / 2
     'The thumbstick position must be more than 1/2 over the neutral end to register as moved.
-    'A short is a signed 16-bit (2-byte) integer range -32,768 through 32,767. This gives us 65,536 values.
 
     'Set the trigger threshold to 1/4 pull.
     Private Const TriggerThreshold As Byte = 64 '64 = 256 / 4
@@ -87,6 +86,12 @@ Public Class Form1
     'A byte is a unsigned 8-bit (1-byte) integer range 0 through 255. This gives us 256 values.
 
     Private ReadOnly Connected(0 To 3) As Boolean
+
+    Private Const DPadUp As Integer = 1
+    Private Const DPadDown As Integer = 2
+
+    Private Const DPadLeft As Integer = 4
+    Private Const DPadRight As Integer = 8
 
     Private Const StartButton As Integer = 16
     Private Const BackButton As Integer = 32
@@ -168,11 +173,6 @@ Public Class Form1
     Private batteryInfo As XINPUT_BATTERY_INFORMATION
 
     Private Const BATTERY_DEVTYPE_GAMEPAD As Integer = 0
-
-    Private Const DPadUp As Integer = 1
-    Private Const DPadDown As Integer = 2
-    Private Const DPadLeft As Integer = 4
-    Private Const DPadRight As Integer = 8
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
